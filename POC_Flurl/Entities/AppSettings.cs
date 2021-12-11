4296b35b0672420a044f9b0953062a6d39fca6f9
Rafael Souza <rafael.silva.xp@hotmail.com>
2021-12-11 11:06:43 -0300

    Adjust settings

--- POC_Flurl/Entities/AppSettings.cs
+++ POC_Flurl/Entities/AppSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POC_Flurl.Entities
+﻿namespace POC_Flurl.Entities
 {
     public class AppSettings
     {
