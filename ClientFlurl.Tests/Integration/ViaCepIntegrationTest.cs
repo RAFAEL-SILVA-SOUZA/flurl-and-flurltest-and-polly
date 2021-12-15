9dc0b165b70a24629b8b579a688c45977873d2cd
Rafael Souza <rafael.silva.xp@hotmail.com>
2021-12-15 10:38:41 -0300

    Adjust

--- ClientFlurl.Tests/Integration/ViaCepIntegrationTest.cs
+++ ClientFlurl.Tests/Integration/ViaCepIntegrationTest.cs
@@ -30,7 +30,7 @@ namespace ClientFlurl.Tests.Integration
             _logger.Received(1).Log(LogLevel.Error, string.Format(Messages.Error_to_received_response, (int)HttpStatusCode.BadRequest));
         }
 
-        [Fact(DisplayName = "Should return http status code of success and no content")]
+        [Fact(DisplayName = "Should return http status code of success and empty content")]
         public async Task Should_be_get_address_by_cep_no_content()
         {
             //Arrange
@@ -44,7 +44,7 @@ namespace ClientFlurl.Tests.Integration
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
         }
 
-        [Fact(DisplayName = "Should return http status code of success and invalid address")]
+        [Fact(DisplayName = "Should return http status code of success and and empty content")]
         public async Task Should_be_get_address_by_cep_success_and_invalid_address()
         {
             //Arrange
