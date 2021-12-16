cc4fdded68b385f8e5d6c826caaf769d8054b409
Rafael Souza <rafael.silva.xp@hotmail.com>
2021-12-16 15:17:42 -0300

    Adjust

--- ClientFlurl.Tests/IntegrationTest/IntegrationTestBase.cs
+++ ClientFlurl.Tests/IntegrationTest/IntegrationTestBase.cs
@@ -1,20 +1,33 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Net.Http;
 
 namespace ClientFlurl.Tests.IntegrationTest
 {
-    public abstract class IntegrationTestBase<TStartup> : TestBase where TStartup : class
+    public abstract class IntegrationTestBase<TStartup, TIService> : TestBase where TStartup : class
+                                                                              where TIService : class
     {
-        private readonly TestServer _server;
-        protected readonly HttpClient _client;
+        private TestServer _server;
+        protected HttpClient _client;
+        protected TIService _service;
 
         public IntegrationTestBase()
+        {
+            _service = NSubstitute.Substitute.For<TIService>();
+        }
+
+        public void CreateServer()
         {
             // Arrange
             _server = new TestServer(new WebHostBuilder()
                 .UseEnvironment("Test")
+                .ConfigureTestServices(services =>
+                {
+                    services.RemoveAll<TIService>();
+                    services.TryAddScoped(x => _service);
+                })
                 .UseStartup<TStartup>());
 
             //Act
--- ClientFlurl.Tests/IntegrationTest/ViaCepIntegrationTest.cs
+++ ClientFlurl.Tests/IntegrationTest/ViaCepIntegrationTest.cs
@@ -1,19 +1,36 @@
 ﻿using ClientFlurl.Api;
+using ClientFlurl.Entities;
+using ClientFlurl.Services;
 using FluentAssertions;
+using Flurl.Http;
+using NSubstitute;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace ClientFlurl.Tests.IntegrationTest
 {
-    public class ViaCepIntegrationTest : IntegrationTestBase<Startup>
+    public class ViaCepIntegrationTest : IntegrationTestBase<Startup, IViaCepClient>
     {
 
-        [Fact(DisplayName = "Should return internal server error when passed an invalid zip code")]
-        public async Task Raise_exception_on_get_address_by_cep()
+        [Fact(DisplayName = "Should be return badrequest")]
+        public async Task Raise_flurl_exception()
         {
             //Arrange
-            CreateHttpTest(Resources.MockJson.Address_correct);
+            _service.When(x => x.GetAddressByZipCode(Arg.Any<string>())).Do(x =>
+            {
+                var call = new FlurlCall
+                {
+                    Response = new FlurlResponse(new System.Net.Http.HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.BadRequest,
+                    })
+                };
+                throw new FlurlHttpException(call,"",new Exception());
+            });
+
+            CreateServer();
 
             // Act
             var response = await _client.GetAsync("/viacep?cep=XPTO");
@@ -23,11 +40,29 @@ namespace ClientFlurl.Tests.IntegrationTest
         }
 
 
+        [Fact(DisplayName = "Should be return empty")]
+        public async Task Should_be_return_empty()
+        {
+            //Arrange
+            _service.GetAddressByZipCode(Arg.Any<string>()).Returns(default(Address));
+            CreateServer();
+
+            // Act
+            var response = await _client.GetAsync("/viacep?cep=XPTO");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+
         [Fact(DisplayName = "Should return http status code of success and a content of type json")]
         public async Task Should_be_get_address_by_cep()
         {
             //Arrange
             CreateHttpTest(Resources.MockJson.Address_correct);
+
+            _service.GetAddressByZipCode(Arg.Any<string>()).Returns(GetInstanceByJson<Address>(Resources.MockJson.Address_correct));
+            CreateServer();
 
             // Act
             var response = await _client.GetAsync("/viacep?cep=24740500");
